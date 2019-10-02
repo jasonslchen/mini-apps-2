6cc2ce1b3699f26a031a074bfa3b9b12bdba6c87
Jason Chen <jason.sl.chen@gmail.com>
2019-10-02 14:59:48 -0700

    semi-correct formatting of my data after i get it from the server

--- challenge_1/client/eventEntry.jsx
+++ challenge_1/client/eventEntry.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const EventEntry = (props) => {
   const { event } = props;
+  let timePeriod = 'AD';
+  let date;
+  if (event.date[0] === '-') {
+    timePeriod = 'BC';
+    date = event.date.slice(1, 5);
+  } else {
+    date = event.date.slice(0, 4);
+  }
+  date += timePeriod;
+
   return (
     <div>
-      {event.description}
+      <div>
+        {event.category1}
+,
+        {' '}
+        {date}
+      </div>
+      <div>
+Description:
+        {' '}
+        {event.description}
+      </div>
     </div>
   );
 };
