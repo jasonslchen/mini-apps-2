2c94612ce0eed9ce14da53af49c19eb728a21dbd
Jason Chen <jason.sl.chen@gmail.com>
2019-10-02 11:23:26 -0700

    basic file setup

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,23 @@
+import React from 'react';
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({
+
+    });
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>hi</div>
+    );
+  }
+}
+
+export default App;
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /(\.m?js$|\.m?jsx$)/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
