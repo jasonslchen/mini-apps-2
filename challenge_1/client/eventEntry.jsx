4f889ff476e3af379c6e0eb1c4f56234c6187efd
Jason Chen <jason.sl.chen@gmail.com>
2019-10-02 15:43:30 -0700

    pushed to wrong repo, fixed my pushing

--- challenge_1/client/eventEntry.jsx
+++ challenge_1/client/eventEntry.jsx
@@ -2,23 +2,36 @@ import React from 'react';
 
 const EventEntry = (props) => {
   const { event } = props;
+  console.log(event);
   let timePeriod = 'AD';
-  let date;
-  if (event.date[0] === '-') {
+  let { date } = event;
+  if (date[0] === '-') {
     timePeriod = 'BC';
-    date = event.date.slice(1, 5);
-  } else {
-    date = event.date.slice(0, 4);
+    date = date.slice(1);
   }
   date += timePeriod;
 
   return (
     <div>
       <div>
-        {event.category1}
-,
-        {' '}
-        {date}
+        Date: 
+{' '}
+{date}
+      </div>
+      <div>
+        Language: 
+{' '}
+{event.lang}
+      </div>
+      <div>
+        Category: 
+{' '}
+{event.category1}
+      </div>
+      <div>
+        Granularity: 
+{' '}
+{event.granularity}
       </div>
       <div>
 Description:
