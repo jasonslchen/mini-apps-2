c0523e35a3182ab8e591d832e8f5d624087f8dbc
Jason Chen <jason.sl.chen@gmail.com>
2019-10-03 11:44:05 -0700

    set up miniapp2 spring

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const path = require('path');
 
+const port = 3000;
+const app = express();
 
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
