f59cfde4543a3b19c6f9b6e5997cf543be606fab
Jason Chen <jason.sl.chen@gmail.com>
2019-10-04 08:55:46 -0700

    set up basic react ile

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const App = (props) => (
-  <div>Hi</div>
-);
+const App = (props) => {
+  useEffect(() => {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then((data) => {
+        console.log(data);
+      });
+  });
+  return (
+    <div>
+      <div>Coin Desk App</div>
+      <div>Powered by <a href="https://www.coindesk.com/price/bitcoin">CoinDes</a>k</div>
+    </div>
+
+  );
+};
+
 
 export default App;
